b32b0808d0323ba10a4f04cebeabc95d78f67828
BogdanBarylo <barylob@gmail.com>
2023-06-03 19:21:11 +0500

    Second step in project, add parser -f

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,16 +3,19 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser(
-            prog='gendiff',
-            description='Compares two configuration files and shows a difference.')
+    prog='gendiff',
+    description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(args)
-
+    if args.format:
+        print('format change')
+    
 
 def main():
     get_parser()
+
 
 if __name__ == '__main__':
     main()
