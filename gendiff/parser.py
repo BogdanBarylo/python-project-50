cd2eb0011a846fcb33bbca3a5c40e12a0861da3e
BogdanBarylo <barylob@gmail.com>
2023-06-22 01:00:58 +0500

    add parser for deep files, fix tests

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -9,17 +9,23 @@ def find_diff(dict_1, dict_2):
     added_string = [{'key': x, 'value': dict_2.get(x), 'type': 'added'}
                     for x in only_diff_keys_2]
     intersection_string = []
-    for i in intersection_set:
-        if dict_1.get(i) == dict_2.get(i):
-            intersection_string.append({'key': i,
-                                        'value': dict_1.get(i),
+    for key in intersection_set:
+        value_1 = dict_1.get(key)
+        value_2 = dict_2.get(key)
+        if value_1 == value_2:
+            intersection_string.append({'key': key,
+                                        'value': value_1,
                                         'type': 'default'})
+        elif isinstance(value_1, dict) and isinstance(value_2, dict):
+            intersection_string.append({'key': key,
+                                        'value': find_diff(value_1, value_2),
+                                        'type': 'container'})
         else:
-            intersection_string.append({'key': i,
-                                        'value': dict_1.get(i),
+            intersection_string.append({'key': key,
+                                        'value': value_1,
                                         'type': 'removed'})
-            intersection_string.append({'key': i,
-                                        'value': dict_2.get(i),
+            intersection_string.append({'key': key,
+                                        'value': value_2,
                                         'type': 'added'})
     result_string = []
     result_string.extend(removed_string)
--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -1,24 +1,52 @@
 from gendiff.gendiff_engine import generate_diff
 
 
-def test_generate_diff():
-    with open('tests/fixtures/result_test.txt', 'r') as result_file:
-        result_diff = result_file.read()
-    with open('tests/fixtures/result_deep_test.txt', 'r') as result_deep_file:
-        result_deep_diff = result_deep_file.read()
-    file_1_path = "tests/fixtures/file1.json"
-    file_2_path = "tests/fixtures/file2.json"
-    file_3_path = "tests/fixtures/file3.yml"
-    file_4_path = "tests/fixtures/file4.yaml"
-    file_1_deep_path = "tests/fixtures/file1_deep.json"
-    file_2_deep_path = "tests/fixtures/file2_deep.json"
-    file_3_deep_path = "tests/fixtures/file3_deep.yml"
-    file_4_deep_path = "tests/fixtures/file4_deep.yaml"
-    assert generate_diff(file_1_path, file_2_path) == result_diff
-    assert generate_diff(file_3_path, file_4_path) == result_diff
-    assert generate_diff(file_1_path, file_4_path) == generate_diff(file_3_path, file_2_path)
-    assert generate_diff(file_1_path, file_2_path) == generate_diff(file_3_path, file_4_path)
-    assert generate_diff(file_1_deep_path, file_2_deep_path) == result_deep_diff
-    assert generate_diff(file_3_deep_path, file_4_deep_path) == result_deep_diff
-    assert generate_diff(file_1_deep_path, file_4_deep_path) == generate_diff(file_3_deep_path, file_2_deep_path)
-    assert generate_diff(file_1_deep_path, file_2_deep_path) == generate_diff(file_3_deep_path, file_4_deep_path)
+with (
+    open('tests/fixtures/result_test.txt', 'r') as result_file,         # Глеб можно ли выносить это в константу?
+    open('tests/fixtures/result_deep_test.txt', 'r') as result_deep_file):
+    RESULT = result_file.read()
+    DEEP_RESULT = result_deep_file.read()
+FILE_1_PATH = "tests/fixtures/file1.json"               # И пути тоже можно выносить в константу?
+FILE_2_PATH = "tests/fixtures/file2.json"
+FILE_3_PATH = "tests/fixtures/file3.yml"
+FILE_4_PATH = "tests/fixtures/file4.yaml"
+FILE_1_DEEP_PATH = "tests/fixtures/file1_deep.json"
+FILE_2_DEEP_PATH = "tests/fixtures/file2_deep.json"
+FILE_3_DEEP_PATH = "tests/fixtures/file3_deep.yml"
+FILE_4_DEEP_PATH = "tests/fixtures/file4_deep.yaml"
+
+
+def test_generate_diff_json():
+    assert generate_diff(FILE_1_PATH, FILE_2_PATH) == RESULT
+
+
+def test_generate_diff_yml():
+    assert generate_diff(FILE_3_PATH, FILE_4_PATH) == RESULT
+
+
+def test_generate_cross_diff_json_yml():
+    assert generate_diff(FILE_1_PATH, FILE_4_PATH) == (     # Такая запись норм?
+           generate_diff(FILE_3_PATH, FILE_2_PATH))
+
+
+def test_generate_cross_diff_yml_json():
+    assert generate_diff(FILE_1_PATH, FILE_2_PATH) == (     # Есть ли смысл в таком тесте?
+           generate_diff(FILE_3_PATH, FILE_4_PATH))
+
+
+def test_deep_generate_diff_json():
+    assert generate_diff(FILE_1_DEEP_PATH, FILE_2_DEEP_PATH) == DEEP_RESULT
+
+
+def test_deep_generate_diff_yml():
+    assert generate_diff(FILE_3_DEEP_PATH, FILE_4_DEEP_PATH) == DEEP_RESULT
+
+
+def test_deep_generate_cross_diff_json_yml():           #нейминги плохие?
+    assert generate_diff(FILE_1_DEEP_PATH, FILE_3_DEEP_PATH) == (
+           generate_diff(FILE_4_DEEP_PATH, FILE_2_DEEP_PATH))
+
+
+def test_deep_generate_cross_diff_yml_json():
+    assert generate_diff(FILE_1_DEEP_PATH, FILE_2_DEEP_PATH) == (
+           generate_diff(FILE_3_DEEP_PATH, FILE_4_DEEP_PATH))
