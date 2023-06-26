6e1b1acbbb4391c66feade69dcbf810dcd157bdf
BogdanBarylo <barylob@gmail.com>
2023-06-27 02:39:47 +0500

    fix default stylish

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -11,7 +11,8 @@ def main():
                                      description=DESCRIPTION)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output', formater="stylish")
+    parser.add_argument('-f', '--format', help='set format of output',
+                        formater="stylish")
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
