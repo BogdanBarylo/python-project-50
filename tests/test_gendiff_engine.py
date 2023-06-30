b43457706b6d85891d35105277cf4ade22890723
BogdanBarylo <barylob@gmail.com>
2023-07-01 02:25:13 +0500

    fix tests, stylish

--- tests/test_gendiff_engine.py
+++ tests/test_gendiff_engine.py
@@ -16,35 +16,48 @@ result_plain = 'tests/fixtures/result_test_plain.txt'
 result_deep_plain = 'tests/fixtures/result_deep_test_plain.txt'
 result_json = 'tests/fixtures/result_test_json_formater.txt'
 result_json_deep = 'tests/fixtures/result_deep_test_json_formater.txt'
-format = ['stylish', 'plain', 'json']
 
 
-@pytest.mark.parametrize('path_1, path_2, format, result',
-                         [(path_1, path_2, 'stylish', result),
-                          (path_3, path_4, 'stylish', result),
-                          (path_1, path_4, 'stylish', result),
-                          (path_3, path_2, 'stylish', result),
-                          (path_1_d, path_2_d, 'stylish', deep_result),
-                          (path_3_d, path_4_d, 'stylish', deep_result),
-                          (path_1_d, path_4_d, 'stylish', deep_result),
-                          (path_3_d, path_2_d, 'stylish', deep_result),
-                          (path_1, path_2, 'plain', result_plain),
-                          (path_3, path_4, 'plain', result_plain),
-                          (path_1, path_4, 'plain', result_plain),
-                          (path_3, path_2, 'plain', result_plain),
-                          (path_1_d, path_2_d, 'plain', result_deep_plain),
-                          (path_3_d, path_4_d, 'plain', result_deep_plain),
-                          (path_1_d, path_4_d, 'plain', result_deep_plain),
-                          (path_3_d, path_2_d, 'plain', result_deep_plain),
-                          (path_1, path_2, 'json', result_json),
-                          (path_3, path_4, 'json', result_json),
-                          (path_1, path_4, 'json', result_json),
-                          (path_3, path_2, 'json', result_json),
-                          (path_1_d, path_2_d, 'json', result_json_deep),
-                          (path_3_d, path_4_d, 'json', result_json_deep),
-                          (path_1_d, path_4_d, 'json', result_json_deep),
-                          (path_3_d, path_2_d, 'json', result_json_deep)])
-def test_generate_diff(path_1, path_2, format, result):
+@pytest.mark.parametrize('path_1, path_2, result',
+                         [(path_1, path_2, result),
+                          (path_3, path_4, result),
+                          (path_1, path_4, result),
+                          (path_3, path_2, result),
+                          (path_1_d, path_2_d, deep_result),
+                          (path_3_d, path_4_d, deep_result),
+                          (path_1_d, path_4_d, deep_result),
+                          (path_3_d, path_2_d, deep_result)])
+def test_generate_diff_stylish(path_1, path_2, result):
     with open(result) as f:
         true_result = f.read()
-    assert generate_diff(path_1, path_2, format) == true_result
+    assert generate_diff(path_1, path_2, format='stylish') == true_result
+
+
+@pytest.mark.parametrize('path_1, path_2, result',
+                         [(path_1, path_2, result_plain),
+                          (path_3, path_4, result_plain),
+                          (path_1, path_4, result_plain),
+                          (path_3, path_2, result_plain),
+                          (path_1_d, path_2_d, result_deep_plain),
+                          (path_3_d, path_4_d, result_deep_plain),
+                          (path_1_d, path_4_d, result_deep_plain),
+                          (path_3_d, path_2_d, result_deep_plain)])
+def test_generate_diff_plain(path_1, path_2, result):
+    with open(result) as f:
+        true_result = f.read()
+    assert generate_diff(path_1, path_2, format='plain') == true_result
+
+
+@pytest.mark.parametrize('path_1, path_2, result',
+                         [(path_1, path_2, result_json),
+                          (path_3, path_4, result_json),
+                          (path_1, path_4, result_json),
+                          (path_3, path_2, result_json),
+                          (path_1_d, path_2_d, result_json_deep),
+                          (path_3_d, path_4_d, result_json_deep),
+                          (path_1_d, path_4_d, result_json_deep),
+                          (path_3_d, path_2_d, result_json_deep)])
+def test_generate_diff_json(path_1, path_2, result):
+    with open(result) as f:
+        true_result = f.read()
+    assert generate_diff(path_1, path_2, format='json') == true_result
